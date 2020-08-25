a1ae0e01479dcc39124482ffe352b4acbe5392f6
Procaillass <52908811+Procaillass@users.noreply.github.com>
2020-08-25 11:58:19 +0200

    first init

---
+++ service.js
@@ -0,0 +1,22 @@
+const version = '0.0.1'
+
+self.addEventListener('install', event => {
+    console.log('install' + version)
+    return self.skipWaiting();
+})
+
+self.addEventListener('activate', event => {
+    console.log('activate')
+    return self.clients.claim();
+})
+
+self.addEventListener("fetch", event => {
+
+    const requestUrl = new URL (
+
+        event.request.url
+
+    )
+    console.log(requestUrl)
+
+})
