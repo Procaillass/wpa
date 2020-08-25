d41939eda361148e303540dd75aa9d9babbcd389
Procaillass <52908811+Procaillass@users.noreply.github.com>
2020-08-25 14:48:57 +0200

    zer
    sqrfz

--- service.js
+++ service.js
@@ -1,7 +1,7 @@
-const version = '0.0.1'
+const version = '0.0.3'
 
 self.addEventListener('install', event => {
-    console.log('install' + version)
+    console.log('Install' + version)
     return self.skipWaiting();
 })
 
@@ -10,13 +10,40 @@ self.addEventListener('activate', event => {
     return self.clients.claim();
 })
 
-self.addEventListener("fetch", event => {
+// Mise en cache
 
-    const requestUrl = new URL (
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
+if(workbox) {
+    console.log('yay Workbox is loaded')
+
+    workbox.precaching.precacheAndRoute([
+        {
+            url: "index.html"
+        },
+        {
+            url: "style.css"
+        },
+        {
+            url: "images/logo.png"
+        },
+        {
+            url: "main.js"
+        },
+        {
+            url: "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.0/css/bulma.min.css"
+        }
+    ])
+}
+else {
+    console.log("alert", "error Workbox")
+}
+
+
+/*self.addEventListener("fetch", event=> {
+   
+    const requestUrl = new URL(
         event.request.url
-
-    )
-    console.log(requestUrl)
-
-})
+      )
+      console.log(requestUrl)
+})*/
